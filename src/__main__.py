bdf479aa68a7461aafff260426b4b3df4b3563ed
Théo LUDWIG <contact@theoludwig.fr>
2024-01-26 13:05:46 +0100

    feat: improvements prompt (#2)

--- src/__main__.py
+++ src/__main__.py
@@ -3,23 +3,31 @@ import os
 import coconut_ai
 
 if __name__ == '__main__':
-    input_story_theme = "Coconut"
-    steps = 20
+    input_story_theme = "Create a story for children, featuring a little girl named Agathe, who takes the throne of the Snow Queen."
+    input_story_name = "agathe-story"
+    steps = 15
 
-    model_path = os.path.abspath(os.path.join("data", "llama-2-7b.Q5_K_M.gguf"))
+    output_folder_path = os.path.abspath(os.path.join("data", input_story_name))
+    os.makedirs(output_folder_path, exist_ok=True)
+
+    model_path = os.path.abspath(os.path.join("data", "models", "llama-2-7b.Q5_K_M.gguf"))
     comic_prompts = coconut_ai.text_to_text({
-        'input_text': "You will make up the description of a 3 panel comic, telling a specific story. You will describe every panel visually one by one, each one with a single sentence ending with a point. Your response will need to contain nothing other than those four visual description. The story will be of the following theme: " + input_story_theme + ".",
+        'input_text': "You will make up the description of a 3 panel comic, telling a specific story. You will describe every panel visually one by one, each one with a single sentence ending with the character \"-\" to clearly separate every description of each panel. Your response will need to contain nothing other than those 3 visual descriptions. No more than 3 sentences to describe this comic. Also, the description of each panel will need to contain every information needed to indenpendently create the visual representation of them, so dont replace nouns with personal pronouns. The story will be about the following theme: " + input_story_theme + ".",
         'max_tokens': 1000,
         'model_path': model_path
-    }).split(".")
-    prompt_number = 0
-    for prompt in comic_prompts:
-        print("prompt_number: " + str(prompt_number) + ", prompt: " + prompt)
-        output_path = os.path.abspath(os.path.join("data", "comic" + "-" + str(prompt_number) + ".png"))
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        coconut_ai.text_to_image({
-            "input_text": prompt,
-            "output_path": output_path,
-            "steps": steps
-        })
-        prompt_number = prompt_number + 1
+    }).split("-")
+
+    with open(os.path.join(output_folder_path, "story.txt"), "w", encoding="utf-8") as file:
+        file.write(input_story_theme + "\n\n")
+        for prompt_index in range(len(comic_prompts)):
+            prompt = comic_prompts[prompt_index]
+            output_text = "prompt_number: " + str(prompt_index + 1) + ", prompt: " + prompt + "\n"
+            print(output_text)
+            file.write(output_text)
+
+            output_image_path = os.path.abspath(os.path.join(output_folder_path, "comic" + "-" + str(prompt_index + 1) + ".png"))
+            coconut_ai.text_to_image({
+                "input_text": prompt,
+                "output_path": output_image_path,
+                "steps": steps
+            })
