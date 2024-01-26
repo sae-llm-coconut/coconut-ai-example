e20c5cfb16119ba7ffea26edf08b2cdf52759ecc
Théo LUDWIG <contact@theoludwig.fr>
2024-01-26 09:20:42 +0100

    feat: generate comics (#1)
    Co-authored-by: Maxime Rumpler <maxime.rumpler@etu.unistra.fr>

--- src/__main__.py
+++ src/__main__.py
@@ -1,40 +1,25 @@
 import os
-import argparse
 
 import coconut_ai
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser("coconut-ai")
-    parser.add_argument("--type", help="The types of conversion you want to do.", choices=["text_to_image", "image_to_text", "text_to_text", "image_to_image"], required=True)
-    parser.add_argument("--input", help="The prompt or image input.", type=str, required=True)
-    parser.add_argument("--output", help="The output path.", type=str, required=True)
-    parser.add_argument("--steps", help="The number of steps.", type=int, default=5, required=False)
-    arguments = parser.parse_args()
+    input_story_theme = "Coconut"
+    steps = 20
 
-    steps = arguments.steps
-
-    if arguments.type == "text_to_image":
-        output_path = os.path.abspath(arguments.output)
+    model_path = os.path.abspath(os.path.join("data", "llama-2-7b.Q5_K_M.gguf"))
+    comic_prompts = coconut_ai.text_to_text({
+        'input_text': "You will make up the description of a 3 panel comic, telling a specific story. You will describe every panel visually one by one, each one with a single sentence ending with a point. Your response will need to contain nothing other than those four visual description. The story will be of the following theme: " + input_story_theme + ".",
+        'max_tokens': 1000,
+        'model_path': model_path
+    }).split(".")
+    prompt_number = 0
+    for prompt in comic_prompts:
+        print("prompt_number: " + str(prompt_number) + ", prompt: " + prompt)
+        output_path = os.path.abspath(os.path.join("data", "comic" + "-" + str(prompt_number) + ".png"))
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        print("---Text To Image---")
-        print(f"Text: \"{arguments.input}\"")
-        print(f"Image Output Path: \"{output_path}\"")
-        print(f"Steps: {steps}")
         coconut_ai.text_to_image({
-            "input_text": arguments.input,
+            "input_text": prompt,
             "output_path": output_path,
             "steps": steps
         })
-    elif arguments.type == "image_to_image":
-        input_path = os.path.abspath(arguments.input)
-        output_path = os.path.abspath(arguments.output)
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        print("---Image To Image---")
-        print(f"Image Input Path: \"{input_path}\"")
-        print(f"Image Output Path: \"{output_path}\"")
-        print(f"Steps: {steps}")
-        coconut_ai.image_to_image({
-            "input_path": input_path,
-            "output_path": output_path,
-            "steps": steps
-        })
+        prompt_number = prompt_number + 1
